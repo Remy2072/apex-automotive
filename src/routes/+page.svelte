1826a5c7a491a8336213763cfefc8041412b3a14
Remy Duivesteijn <remyd03@icloud.com>
2025-12-13 14:28:27 +0100

    project start-up

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<section>
+	<div class="container">hallo</div>
+</section>
+
+<style>
+	.container {
+		max-width: 1120px;
+		margin: 0 auto;
+		padding: 16px;
+	}
+
+	div {
+		width: 100%;
+		background-color: blueviolet;
+		height: 40px;
+	}
+</style>
