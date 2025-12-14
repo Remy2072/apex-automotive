008911f2310ad5b87aeaa3bef04262a08ad95b9d
Remy Duivesteijn <remyd03@icloud.com>
2025-12-14 20:44:47 +0100

    .2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { page } from '$app/state ';
+	import { page } from '$app/stores';
 	import { locales, localizeHref } from '$lib/i18n/paraglide/runtime';
 	import '../app.css';
 	import './layout.css';
